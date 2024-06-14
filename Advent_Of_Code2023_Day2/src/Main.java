8a82148e4c58b90f54308e91f05fef3f4865a54f
TymonScibiorski <tym9128@gmail.com>
2024-06-14 15:23:24 +0200

    Day two of advent of code 2023

--- Advent_Of_Code2023_Day2/src/Main.java
+++ Advent_Of_Code2023_Day2/src/Main.java
@@ -21,30 +21,70 @@ public class Main {
     }
 
     public static int finalFunction(String line){
-//        if (isPossible(line)){
-//            return getID(line);
-//        }
-        return 0;
+        String[] gameIntAndSets = splitAtColons(line);
+        String[] Sets = splitAtSemicolons(gameIntAndSets[1]);
+        for(int i = 0; i<Sets.length; i++){
+            if(!isPossible(Sets[i])){
+                return 0;
+            }
+        }
+        return getID(gameIntAndSets[0]);
     }
 
     public static boolean isPossible(String SingleSet){
         String[] data = SingleSet.split(", ");
-        for (int i = 0; i<data.length; i++){
-            if (data[i].matches("red")){
-                int amountOfRedCubes = Integer.valueOf(String.valueOf(data[i].matches("\\d")));
+        for (int i = 0; i<data.length; i++) {
+            if (12 < amountOfRed(data, i)) {
+                return false;
             }
-            if (data[i].matches("")){
 
+            if (13 < amountOfGreen(data, i)) {
+                return false;
             }
-            if (data[i].matches("")){
 
+            if(14 < amountOfBlue(data, i)){
+                return false;
             }
         }
-
+        return true; //if none of the subsets exceeded the limit of 12 red, 13 green or 14 blue, then the game is possible
     }
 
-    public static String[] extractData(String line){
-        return new String[]{ExtractGameNumber(splitAtColons(line)[0]), }
+    public static int amountOfRed(String[] data, int i){
+        if (IsRed(data, i)){
+                return Integer.parseInt(data[i].split(" ")[0]);
+            }
+        return 0;
+    }
+    public static int amountOfGreen(String[] data, int i){
+        if (IsGreen(data, i)){
+                return Integer.parseInt(data[i].split(" ")[0]);
+            }
+        return 0;
+    }
+    public static int amountOfBlue(String[] data, int i){
+        if (IsBlue(data, i)){
+                return Integer.parseInt(data[i].split(" ")[0]);
+            }
+        return 0;
+    }
+
+    public static boolean IsRed(String[] data, int i){
+        if (data[i].matches("\\d+ red")){
+            return true;
+        }
+        return false;
+    }
+    public static boolean IsGreen(String[] data, int i){
+        if (data[i].matches("\\d+ green")){
+            return true;
+        }
+        return false;
+    }
+    public static boolean IsBlue(String[] data, int i){
+        if (data[i].matches("\\d+ blue")){
+            return true;
+        }
+        return false;
     }
 
     public static String[] splitAtSemicolons(String line){
@@ -55,7 +95,7 @@ public class Main {
         return line.split(": ");
     }
 
-    public static int ExtractGameNumber(String GameInt){
+    public static int getID(String GameInt){
         return Integer.parseInt(GameInt.substring(GameInt.length()-1));
     }
 }
--- Advent_Of_Code2023_Day2/src/MainTest.java
+++ Advent_Of_Code2023_Day2/src/MainTest.java
@@ -4,8 +4,58 @@ import org.junit.jupiter.api.Test;
 class MainTest {
 
     @Test
+    void TestFinalFunction(){
+        Assertions.assertEquals(1, Main.finalFunction("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"));
+        Assertions.assertEquals(2, Main.finalFunction("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"));
+        Assertions.assertEquals(5, Main.finalFunction("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"));
+
+        Assertions.assertEquals(0, Main.finalFunction("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"));
+        Assertions.assertEquals(0, Main.finalFunction("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red"));
+    }
+
+    @Test
+    void TestIsPossible(){
+        Assertions.assertTrue(Main.isPossible("3 blue, 4 red"));
+        Assertions.assertTrue(Main.isPossible("1 red, 2 green, 6 blue"));
+        Assertions.assertTrue(Main.isPossible("2 green"));
+        Assertions.assertTrue(Main.isPossible("1 blue"));
+        Assertions.assertTrue(Main.isPossible("2 green"));
+        Assertions.assertTrue(Main.isPossible("3 green, 4 blue, 1 red"));
+        Assertions.assertTrue(Main.isPossible("1 green, 1 blue"));
+        Assertions.assertTrue(Main.isPossible("6 red, 1 blue, 3 green"));
+        Assertions.assertTrue(Main.isPossible("2 blue, 1 red, 2 green"));
+        Assertions.assertTrue(Main.isPossible("5 blue, 4 red, 13 green"));
+        Assertions.assertTrue(Main.isPossible("5 green, 1 red"));
+        Assertions.assertFalse(Main.isPossible("8 green, 6 blue, 20 red"));
+        Assertions.assertFalse(Main.isPossible("15 blue, 14 red"));
+    }
+
+    @Test
+    void TestIsRed(){
+        Assertions.assertTrue(Main.IsRed(new String[]{"3 blue", "4 red"}, 1));
+        Assertions.assertTrue(Main.IsRed(new String[]{"8 green", "6 blue", "20 red"}, 2));
+        Assertions.assertFalse(Main.IsRed(new String[]{"8 green", "6 blue", "20 red"}, 1));
+    }
+
+    @Test
+    void TestIsBlue(){
+        Assertions.assertTrue(Main.IsBlue(new String[]{"3 blue", "4 red"}, 0));
+        Assertions.assertTrue(Main.IsBlue(new String[]{"8 green", "6 blue", "20 red"}, 1));
+        Assertions.assertFalse(Main.IsBlue(new String[]{"8 green", "6 blue", "20 red"}, 0));
+    }
+
+    @Test
+    void TestIsGreen(){
+        Assertions.assertFalse(Main.IsGreen(new String[]{"3 blue", "4 red"}, 0));
+        Assertions.assertTrue(Main.IsGreen(new String[]{"8 green", "6 blue", "20 red"}, 0));
+        Assertions.assertFalse(Main.IsGreen(new String[]{"8 green", "6 blue", "20 red"}, 2));
+    }
+
+
+    @Test
     void TestSplitAtColons(){
-        Assertions.assertArrayEquals(new String[]{"Game 2", "1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"}, Main.splitAtColons("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"));
+        Assertions.assertArrayEquals(new String[]{"Game 2", "1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"},
+                Main.splitAtColons("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"));
     }
 
     @Test
